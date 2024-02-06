abc1457e631575a0501d16dded810e7fe10c04b0
jeanclaude <Jeanclaude.aoun@hotmail.com>
2024-02-07 03:22:22 +0400

    added tokenomics

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
+const withMDX = require('@next/mdx')()
+ 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  // Configure `pageExtensions` to include MDX files
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  // Optionally, add any other Next.js config below
 }
-
-module.exports = nextConfig
+ 
+module.exports = withMDX(nextConfig)
