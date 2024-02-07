a3f85963989c811291a9fa6b0514c939a2e3477c
jeanclaude <Jeanclaude.aoun@hotmail.com>
2024-02-07 23:21:44 +0400

    updated links

--- components/ui/Navbar/Navbar.jsx
+++ components/ui/Navbar/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
     const navigation = [
         { name: "Features", href: "/#features" },
         { name: "Whitepaper", href: "whitepaper" },
-        { name: "Github", href: "https://github.com/JayDaryn/ordinal-rollup" },
+        { name: "Github", href: "https://github.com/JayDaryn/ordinal-rollup-core" },
     ]
 
     useEffect(()=>{
